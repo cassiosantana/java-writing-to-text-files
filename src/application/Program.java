622bc8555a06b4bca7e76261ef05c6e378497462
Cassio Santana <cassioo.roger@gmail.com>
2022-03-10 09:37:22 -0300

    Writing text file with classes FileWriter and BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,26 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		String[] names = new String[] {"Anna", "Bob", "Willian", "Peter"};
+		String path = new String("C:\\temp\\out.txt");
+		
+//		The "true" parameter appends the text to the end of the file. 
+//		Without this parameter the file will be recreated.
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) { 
+			
+			for (String name : names) {
+				bw.write(name);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
